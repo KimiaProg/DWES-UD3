383380c897e5d126744fc64ee95fc5d78f4a02f2
KimiaProg <kimia.ehsani.dolati.al@iespoligonosur.org>
2021-10-01 14:04:48 +0200

    ejercico3-4

--- ejercicio4.php
+++ ejercicio4.php
@@ -1,27 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
 <body>
     <?php
-    $a = 2;
-    $b = 1;
-    $c = 100;
-    if ($a > $b && $a>$c) {
-        echo $a;
-    } elseif ($b > $c && $b>$a) {
-        echo $b;
-    } else {
-        echo $c;
-    }
 
     ?>
 </body>
-
 </html>
