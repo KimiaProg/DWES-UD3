372661d2e6389437b7bf3ac62d2498cad736f1d9
KimiaProg <kimia.ehsani.dolati.al@iespoligonosur.org>
2021-10-07 12:07:08 +0200

    ejercicio16

--- ejercicio16.php
+++ ejercicio16.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <?php  
+    <?php
     include 'funciones.inc.php';
-    include_once 'funciones.inc.php';
-    require 'funciones.inc.php';
-    echo multiplicacion(3,7);   
-    
+    include_once 'funciones2.inc.php';
+    require 'funciones2.inc.php';
+    echo multiplicacion(3, 7);
     ?>
 </body>
+
 </html>
